43aa00546e29c67c0b1da260313878852a27ce39
NoNonsense126 <wongyoujing118@gmail.com>
2022-01-09 22:47:34 +0800

    added pulang mengundi

---
+++ pages/work/pulang_mengundi.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import React from "react";
+import main from "../../public/work/pulang_mengundi/main.png";
+import subsidy from "../../public/work/pulang_mengundi/subsidy.jpeg";
+import BackToHomeLink from "../../src/data/components/BackToHomeLink";
+
+interface StoneAndChalkProps {}
+
+const PulangMengundi = ({}: StoneAndChalkProps): JSX.Element => {
+  return (
+    <div className="container py-20">
+      <h1>Pulang Mengundi</h1>
+      <div className="text-gray-400">Ruby on Rails · Open Source</div>
+      <a
+        className="underline text-sm mr-3"
+        href="https://www.pulangmengundi.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Site Link
+      </a>
+      <a
+        className="underline text-sm"
+        href="https://github.com/pulang-mengundi/pulang_mengundi"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github Link
+      </a>
+
+      <BackToHomeLink />
+
+      <div className="pt-3"></div>
+      <ul className="list-disc pl-5">
+        <li className="mt-3">
+          PulangMengundi.com was launched as part of the #pulangmengundi
+          movement on the 13th of April, 2018. ​9th May 2018 marks the
+          completion of this initiative.
+        </li>
+        <li>7,000 carpoolers connected</li>
+        <li>RM75,000 pledged by the public</li>
+        <li>RM30,000 raised for Persatuan Belia Harmoni Malaysia</li>
+        <li>
+          Site published on Huffpost, Quarts, SCMP, Channel news asia, and many
+          other media.
+        </li>
+      </ul>
+
+      <div className="mt-8">
+        <div className="mt-3">
+          <h6>Main</h6>
+          <Image
+            src={main}
+            alt="main"
+            placeholder="blur"
+            sizes="(min-width: 1024px) 1024px, 100vw"
+            layout="responsive"
+          />
+        </div>
+        <div className="mt-3">
+          <h6>Subsidy</h6>
+          <Image
+            src={subsidy}
+            alt="subsidy"
+            placeholder="blur"
+            sizes="(min-width: 1024px) 1024px, 100vw"
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PulangMengundi;
--- src/data/work_summaries.ts
+++ src/data/work_summaries.ts
@@ -1,5 +1,10 @@
 export const workSummaries = [
   {
+    title: "Pulang Mengundi",
+    href: "/work/pulang_mengundi",
+    body: `- Founding member and lead developer of an open source and neutral platform that aims to connect Malaysians with other Malaysians to help them travel home to vote. Successfully connected more than 10,000 Malaysian and matched more than RM105,000 from donors to people who needed help.`,
+  },
+  {
     title: "SmithKit",
     href: "/work/smithkit",
     body: `- Fullstack development on a web based meal planner SAAS.
@@ -10,11 +15,12 @@ export const workSummaries = [
     title: "Tayarlo",
     href: "/work/tayarlo",
     body: `- Lead and primary developer for a custom built ERP system. Sales and distribution module, 
-    Material management module with automated warehouse bin assignment, Fleet tracking and dispatch module, Contact center module, warranty management module`,
+    Material management module with automated warehouse bin assignment, Fleet tracking and dispatch module, Contact center module, warranty management module.`,
   },
+
   {
     title: "Stone & Chalk",
     href: "/work/stoneandchalk",
-    body: `- Implemented a beautifully designed website and a digital hub for users of Stone & Chalk`,
+    body: `- Implemented a beautifully designed website and a digital hub.`,
   },
 ];
