d26262a278c108502c3858346deb257ad918970c
You Jing Wong <wongyoujing118@gmail.com>
2025-02-25 10:53:09 +0800

    Change font to outfit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,7 @@ class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
           <link
